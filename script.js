1e37021257f4aa4114fe9e2f34708497ccc0eccd
Filip Kníže <filipknize@gmail.com>
2024-02-22 16:04:11 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var backgrounds = [
+  'background/background1.png',
+  'background/background2.png',
+  'background/background3.png',
+  'background/background4.png',
+  'background/background5.png',
+  'background/background6.png',
+  'background/background7.png'
+];
+
+// vybere náhodnou tapetu
+var randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+// nastavit proměnnou na pozadí
+var body = document.getElementById('randomBg');
+
+// nastaví náhodné pozadí
+body.style.backgroundImage = 'url(' + backgrounds[randomIndex] + ')';
+
+function copyToClipboard() {
+  const textToCopy = "95.82.185.15:25565";
+  const dummyInput = document.createElement("textarea");
+  document.body.appendChild(dummyInput);
+  dummyInput.value = textToCopy;
+  dummyInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummyInput);
+
+  const message = document.getElementById("Popup");
+  message.style.display = "block";
+
+  setTimeout(function() {
+    message.style.display = "none";
+  }, 1000);
+};
+
+function Popup() {
+  const message = document.getElementById("Popup");
+  message.style.display = "block";
+
+  setTimeout(function() {
+    message.style.display = "none";
+  }, 1500);
+};
+
+function getPlayerCount() {
+  const serverIP = '95.82.185.15:25565';
+  const url = `https://api.mcsrvstat.us/2/${serverIP}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.online) {
+      const playerCount = data.players.online;
+      document.getElementById('playerCount').innerText = `Počet aktivních hráčů: ${playerCount}`;
+    } else {
+      document.getElementById('playerCount').innerText = 'Server je offline';
+    }
+  })
+  .catch(error => {
+    console.log('Chyba hledání počtu hráčů:', error);
+    document.getElementById('playerCount').innerText = 'Chyba hledání počtu hráčů';
+  });
+}
+
+// Call getPlayerCount function when the page loads
+window.onload = getPlayerCount;
